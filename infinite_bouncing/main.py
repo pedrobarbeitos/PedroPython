199838718c397ff82ac6a2fe4de6a000100eaddb
Pedro <barbeitos.pedro@gmail.com>
2023-12-16 19:13:30 +0000

    some refactoring

--- infinite_bouncing/main.py
+++ infinite_bouncing/main.py
@@ -1,23 +1,11 @@
-# WRITE YOUR SOLUTION HERE:
-
-# # WRITE YOUR SOLUTION HERE:
-
-# WRITE YOUR SOLUTION HERE:
-
-# WRITE YOUR SOLUTION HERE:
-
-# WRITE YOUR SOLUTION HERE:
-# WRITE YOUR SOLUTION HERE:
 
 import pygame
 import random
 
 
-# Initialize
 pygame.init()
 window = pygame.display.set_mode((640, 480))
 
-# Load image of roboto
 ball = pygame.image.load("ball.png")
 
 x = 0
@@ -27,19 +15,15 @@ velocityY = 2
 clock = pygame.time.Clock()
 
 
-# Game Main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
-    # Window Background color
     window.fill((0, 0, 0))
 
-    # Draw my ball
     window.blit(ball, (x, y))
 
-    # Update the display
     pygame.display.flip()
 
     x += velocityX
